1f31965af486333dccf632c6ce1bb24f1638294d
Dimitri <dimitri.hautot@proxistore.com>
2017-01-12 13:46:53 +0100

    Deleted TripVerticle, because it is currently unused

--- src/main/java/net/trackme/server/ServerApplication.java
+++ src/main/java/net/trackme/server/ServerApplication.java
@@ -1,9 +1,8 @@
 package net.trackme.server;
 
 import io.vertx.core.Vertx;
-import net.trackme.server.verticle.PersistenceVerticle;
+import net.trackme.server.verticle.Persistence;
 import net.trackme.server.verticle.ServerVerticle;
-import net.trackme.server.verticle.TripVerticle;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,8 +22,7 @@ public class ServerApplication {
 	@PostConstruct
     public void deployVerticles() {
 	    final Vertx vertx = Vertx.vertx();
-	    vertx.deployVerticle(new PersistenceVerticle());
-	    vertx.deployVerticle(new TripVerticle());
+	    vertx.deployVerticle(new Persistence());
 	    vertx.deployVerticle(new ServerVerticle(serverPort));
     }
 }
--- src/main/java/net/trackme/server/verticle/TripVerticle.java
+++
@@ -1,9 +0,0 @@
-package net.trackme.server.verticle;
-
-import io.vertx.core.AbstractVerticle;
-
-/**
- * @author Dimitri (09/01/2017)
- */
-public class TripVerticle extends AbstractVerticle {
-}
